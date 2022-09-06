89d024e1afd78f9cd28f59c17f16c47c2b006ff3
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-06 10:09:46 +0530

    Added Emp presented or not

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class EmployeeWage
+{
+	public void employeeIsPresentOrNot() {}
+
+	public static void main(String[] args)
+	{
+		double random = Math.random();
+		System.out.println(random);
+		for (random=0.5; random <= 5; random++ )
+		{
+			System.out.println("emp is present");
+			{
+				System.out.println("emp is Absent");
+			}
+		}
+
+	}
+}
+
