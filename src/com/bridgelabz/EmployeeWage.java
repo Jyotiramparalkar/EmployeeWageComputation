963949464f962b4eb88e8bcdd8ac15c4ce06dd4b
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-07 23:42:11 +0530

    Refactor EmployeeWageUC6

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,26 +1,82 @@
 package com.bridgelabz;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 public class EmployeeWage {
+    private String companyName;
+    private int wagePerHr;
+    private int totalWorkingDays;
+    private int totalWorkingHrs;
+    private int totalEmpWage;
 
-	public static void main(String[] args) 
-	{
-		Random random = new Random();
-		int attendance = random.nextInt(3);
-		
-		int wagePerHr = 20;
-		int fullDayHours = 8;
-		int wagePerDay = 0;
-		if(attendance==2)
-		{
-			System.out.println("Emp is Present");
-			wagePerDay = wagePerHr * fullDayHours;
-		}
-		else
-		{
-		    System.out.println("Emp is absent... :::");	
-		}
-	}
+    private ArrayList<Integer> dailyWages;
 
+public EmployeeWage(String companyName, int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
+        this.companyName = companyName;
+        this.wagePerHr = wagePerHr;
+        this.totalWorkingDays = totalWorkingDays;
+        this.totalWorkingHrs = totalWorkingHrs;
+        dailyWages = new ArrayList<>();
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getWagePerHr() {
+        return wagePerHr;
+    }
+
+    public void setWagePerHr(int wagePerHr) {
+        this.wagePerHr = wagePerHr;
+    }
+
+    public int getTotalWorkingDays() {
+        return totalWorkingDays;
+    }
+
+    public void setTotalWorkingDays(int totalWorkingDays) {
+        this.totalWorkingDays = totalWorkingDays;
+    }
+
+    public int getTotalWorkingHrs() {
+        return totalWorkingHrs;
+    }
+
+    public void setTotalWorkingHrs(int totalWorkingHrs) {
+        this.totalWorkingHrs = totalWorkingHrs;
+    }
+
+    public int getTotalEmpWage() {
+        return totalEmpWage;
+    }
+
+    public void setTotalEmpWage(int totalEmpWage) {
+        this.totalEmpWage = totalEmpWage;
+    }
+
+    public ArrayList<Integer> getDailyWages() {
+        return dailyWages;
+    }
+
+    public void setDailyWages(ArrayList<Integer> dailyWages) {
+        this.dailyWages = dailyWages;
+    }
+
+    public String toString()
+    {
+        return 
+        		"CompanyEmpWage{" +
+                "companyName='" + companyName  +
+                ", wagePerHr=" + wagePerHr +
+                ", totalWorkingDays=" + totalWorkingDays +
+                ", totalWorkingHrs=" + totalWorkingHrs +
+                ", totalEmpWage=" + totalEmpWage +
+                ", dailyWages=" + dailyWages +
+                '}';
+    }
 }
