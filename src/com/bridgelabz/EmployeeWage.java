673bfdcc19549fbb736203c51f16ce1701900955
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-16 20:24:37 +0530

    Update EmployeeWage UC6 creating object with method

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -33,6 +33,9 @@ public class EmployeeWage
 
     public static void main(String[] args)
     {
+    	
+    	
+    	
         System.out.println("Welcome to Employee Wage Computation Program");
 
         EmployeeWage employeeWage = new EmployeeWage();
